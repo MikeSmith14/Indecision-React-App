ead9f5dadd6979a63843ffb31d9b91e350121bc9
Michael Smith <MSmith@soboba-nsn.gov>
2020-10-21 12:07:01 -0700

    Fixing dependencies for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
-const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+//export an object directly from webpack.js or we can export a function and that function when called returns the web config object 
+// so we can access to the varialbes env, argv through module.exports = funciton(env, argv) {}
 module.exports = (env) => {
-    const isProduction = env === 'production'
-    const CSSExtract = new ExtractTextPlugin('styles.css')
+    const isProduction = env === 'production'; // if env not equal to production, this will be false, and we'll run webpack without production optimization
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public', 'dist'),
+            path: path.join(__dirname, 'public'),
             filename: 'bundle.js'
         },
         module: {
-            rules: [{
+            rules: [{   //only when file ending in .js, then we run babel-loader
                 loader: 'babel-loader',
-                test: /\.js$/,
+                test: /\.js$/,           
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use: CSSExtract.extract({
-                    use: [
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
                         }
-                    ]
-                })
-            }
-            ]
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
         },
         plugins: [
             CSSExtract
         ],
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devtool: isProduction ? 'source-map' : 'inline-source-map', // if it's not production mode, we still manna use cheap-module
         devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            publicPath: '/dist/'
+            contentBase: path.join(__dirname, 'public')
         }
     }
-};
+}
